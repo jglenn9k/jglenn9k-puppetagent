350c5e18babe3b5847110df3545f77cf28efd3ad
James Glenn <thedonkdonk@gmail.com>
2015-05-11 19:23:35 -0500

    Config file for Debian

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,7 +7,7 @@ class puppetagent::config inherits puppetagent {
                 path    => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf',
                 owner   => 'SYSTEM',
                 mode    => '0664',
-                content => template('puppet/puppet.conf.windows.erb'),
+                content => template('puppet/puppet.conf.erb'),
                 notify  => Service['puppet']
             }
         }
@@ -17,12 +17,19 @@ class puppetagent::config inherits puppetagent {
                 owner   => 'root',
                 group   => 'root',
                 mode    => '0664',
-                content => template('puppet/puppet.conf.RedHat.erb'),
+                content => template('puppet/puppet.conf.erb'),
                 notify  => Service['puppet']
             }
         }
         'Debian': {
-            fail("TODO: Add support for ${::osfamily}. Feel free to send a pull request.")
+            file {'puppet.conf':
+                path    => '/etc/puppet/puppet.conf',
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0664',
+                content => template('puppet/puppet.conf.erb'),
+                notify  => Service['puppet']
+            }
         }
         default: {
             fail("Module ${module_name} is not currently supported on ${::osfamily}. Feel free to send a pull request.")
